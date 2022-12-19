13dc7ac1cd92cc4bbc4cca274bdc17829fe3ab99
Ed Paradis <eparadis@users.noreply.github.com>
2022-12-18 20:20:22 -0800

    subscribe to multiple topics

--- mqtt.py
+++ mqtt.py
@@ -1,22 +1,37 @@
 from umqtt.simple import MQTTClient
 import ubinascii
 import machine
+import time
 
 # Default MQTT server to connect to
 SERVER = "192.168.0.69"
 CLIENT_ID = ubinascii.hexlify(machine.unique_id())
-TOPIC = b"sensors/greenhouse/temperature"
+
+store = { b"sensors/greenhouse/temperature": 0,
+  b"sensors/greenhouse/humidity/relative": 0,
+  b"sensors/greenhouse/light": 0,
+  b"sensors/greenhouse/vapor_pressure_difference": 0
+}
 
 def sub_cb(topic, msg):
-    print((topic, msg))
+    if topic not in store:
+        print("unexpected subscription message from topic '%s'" % (topic))
+    else:
+        store[topic] = msg
+        print((topic, msg))
 
-def main(server=SERVER):
-    c = MQTTClient(CLIENT_ID, server)
+def main():
+    c = MQTTClient(CLIENT_ID, SERVER)
     # Subscribed messages will be delivered to this callback
     c.set_callback(sub_cb)
     c.connect()
-    c.subscribe(TOPIC)
-    print("Connected to %s, subscribed to %s topic" % (server, TOPIC))
+    print("Connected to %s" % (SERVER,))
+    for topic in store.keys():
+        print("Subscribing to topic '%s'" % (topic))
+        c.subscribe(topic)
+        print("...subscribed. Throttling for 1 second")
+        time.sleep(1)
+    print("Initialization complete")
 
     try:
         while 1:
